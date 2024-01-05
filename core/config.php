55a341cd0dfb426119a7ddc53681a122470270ab
ganesh-kumar-cgk <ganesh@refreshtechlabs.com>
2024-01-05 15:59:24 +0530

    first commit

--- core/config.php
+++ core/config.php
@@ -1,13 +1,15 @@
 <?php
     session_start();
-    $dbname="u399040897_fireflycracker";
-    $hostname="localhost";
-    $username="u399040897_fireflycracker";
-    $password="firefly@Crackers.1234";
-    $siteurl="https://fireflycrackers.com/demo/ff-admin/";
-    $mainurl="https://fireflycrackers.com/demo/";
-    $conn = mysqli_connect($hostname, $username, $password, $dbname);
-    if(!$conn){
-        echo "Failed To Conect:";
+    if($_SERVER['HTTP_HOST'] == "fireflycrackers.com"){
+        $dbname="u399040897_fireflycracker";
+        $hostname="localhost";
+        $username="u399040897_fireflycracker";
+        $password="firefly@Crackers.1234";
+        $siteurl="https://fireflycrackers.com/demo/ff-admin/";
+        $mainurl="https://fireflycrackers.com/demo/";
+        $conn = mysqli_connect($hostname, $username, $password, $dbname);
+        if(!$conn){
+            echo "Failed To Conect:";
+        }
     }
 ?>
