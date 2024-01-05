41e1675dcf7426f8db53c664722505a2529db99e
ganesh-kumar-cgk <ganesh@refreshtechlabs.com>
2024-01-05 16:47:31 +0530

    first commit

--- core/config.php
+++ core/config.php
@@ -24,4 +24,16 @@
             echo "Failed To Conect:";
         }        
     }
+    if($_SERVER['HTTP_HOST'] == "kayatharcrackers.com"){
+        $dbname="u399040897_kayathar";
+        $hostname="localhost";
+        $username="u399040897_kayathar";
+        $password="kayathar@4321@2023KK";
+        $siteurl="https://kayatharcrackers.com/kayatharad/";
+        $mainurl="https://kayatharcrackers.com/demo/";
+        $conn = mysqli_connect($hostname, $username, $password, $dbname);
+        if(!$conn){
+            echo "Failed To Conect:";
+        }        
+    }
 ?>
